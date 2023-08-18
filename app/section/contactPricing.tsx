c1b757cb732d0598cb3abdff180f590f92ec429e
rikoadii <radi60372@gmail.com>
2023-08-19 01:51:58 +0800

    update about

--- app/section/contactPricing.tsx
+++ app/section/contactPricing.tsx
@@ -3,6 +3,7 @@ import { Cols, Rows } from '../layout/direction'
 import Title from '../components/typography/title'
 import Image from 'next/image'
 import Button from '../components/button'
+import Link from 'next/link'
 
 export default function ContactPricing() {
     return (
@@ -12,11 +13,13 @@ export default function ContactPricing() {
                     <Title variantI={2} className='mb-5 w-[75%]'>
                         Confused about which plan would be most suitable for me?
                     </Title>
-                    <Button variant={"primary"}  className='max-w-[30%] text-center'>
-                        Contact us
+                    <Button variant={"primary"} className='max-w-[30%] text-center'>
+                        <Link href={"/contact"} >
+                            Contact us
+                        </Link>
                     </Button>
                 </Cols>
-            <Image src={"Pricing/Group 48097566.svg"} alt='contact pricing' width={428} height={275} />
+                <Image src={"Pricing/Group 48097566.svg"} alt='contact pricing' width={428} height={275} />
             </Rows>
         </Rows>
     )
